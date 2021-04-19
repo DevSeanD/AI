f166633fe92de1d393292c10dc5bdbd0ae0e0784
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-04-18 20:02:44 -0400

    Added Additional Evaluation Function
    Added another evaluation function that takes into consideration both location values and sequence values

--- GameStateAdvantageEvaluation/Connect4Evaluation.py
+++ GameStateAdvantageEvaluation/Connect4Evaluation.py
@@ -33,6 +33,7 @@ class connect4Game:
             print()
 
     def checkWin(self):
+        # This function returns win values based on black being the primary player. So when black loses a small number is returned and when black wins a large number is returned
         bVertWin = 0 # vertical counters
         rVertWin = 0
         for col in range(7):
@@ -46,9 +47,9 @@ class connect4Game:
                 if self.gameBoard[row][col] == '-' or self.gameBoard[row][col] == 'R':
                     bVertWin = 0
                 if bVertWin >= 4:
-                    return("Black has won!")
+                    return 1000
                 if rVertWin >= 4:
-                    return("Red has won!")
+                    return -1000
 
         bHoriWin = 0 # horizontal counters
         rHoriWin = 0
@@ -63,9 +64,9 @@ class connect4Game:
                 if self.gameBoard[row][col] == '-' or self.gameBoard[row][col] == 'R':
                     bHoriWin = 0
                 if bHoriWin >= 4:
-                    return("Black has won!")
+                    return 1000
                 if rHoriWin >= 4:
-                    return("Red has won!")
+                    return -1000
 
         bDiagWin = 0 # diagonal counters
         rDiagWin = 0
@@ -93,12 +94,11 @@ class connect4Game:
                 if self.gameBoard[min(ROW, line) - j - 1][start_col + j] == '-' or self.gameBoard[min(ROW, line) - j - 1][start_col + j] == 'B':
                     rDiagWin = 0
                 if bDiagWin >= 4:
-                    return("Black has won!")
+                    return 1000
                 if rDiagWin >= 4:
-                    return("Red has won!")
+                    return -1000
 
-
-def c4Eval(state):
+def c4Eval(state): # Eval function that takes into consideration value based locations
     # value of each space
     evalTable = [[3,4,5,7,5,4,3],[4,6,8,10,8,6,4],[5,8,11,13,11,8,5],[5,8,11,13,11,8,5],[4,6,8,10,8,6,4],[3,4,5,7,5,4,3]]
 
@@ -111,30 +111,128 @@ def c4Eval(state):
                 rTotal += evalTable[row][col]
             if state[row][col] == 'B':
                 bTotal += evalTable[row][col]
-    
+
     if(rTotal > bTotal):
         print("Red has the avantage of",rTotal,"to",bTotal)
-        return -1000
     if(bTotal > rTotal):
         print("Black has the avantage of",bTotal,"to",rTotal)
-        return 1000
     if(bTotal == rTotal):
         print("Neither player has the advantage")
-        return 0
-    
-    # return rTotal - bTotal
+
+    # heurisic for min-max alg from the point of view of black
+    return bTotal - rTotal
+
+def c4Eval1(state): # Eval function that takes into consideration both value locations and opportunities to win
+    evalTable = [[3,4,5,7,5,4,3],[4,6,8,10,8,6,4],[5,8,11,13,11,8,5],[5,8,11,13,11,8,5],[4,6,8,10,8,6,4],[3,4,5,7,5,4,3]]
+
+    bValTotal = 0
+    rValTotal = 0
+
+    # value based eval
+    for row in range(6):
+        for col in range(7):
+            if state[row][col] == 'R':
+                rValTotal += evalTable[row][col]
+            if state[row][col] == 'B':
+                bValTotal += evalTable[row][col]
+
+
+    # keeps track of piece sequence totals
+    bSeqTotal = 0
+    rSeqTotal = 0
+    # who has move peieces in a row,col,or diagonal
+    bVertWin = 0 # vertical counters
+    rVertWin = 0
+    for col in range(7):
+        for row in range(6):
+            if state[row][col] == 'B':
+                bVertWin += 1
+            if state[row][col] == 'R':
+                rVertWin += 1
+            if state[row][col] == '-' or state[row][col] == 'B':
+                rVertWin = 0
+            if state[row][col] == '-' or state[row][col] == 'R':
+                bVertWin = 0
+            if bVertWin > rVertWin:
+                bSeqTotal += bVertWin
+            if rVertWin > bVertWin:
+                rSeqTotal += rVertWin
+
+        bHoriWin = 0 # horizontal counters
+        rHoriWin = 0
+        for row in range(6):
+            for col in range(7):
+                if state[row][col] == 'B':
+                    bHoriWin += 1
+                if state[row][col] == 'R':
+                    rHoriWin += 1
+                if state[row][col] == '-' or state[row][col] == 'B':
+                    rHoriWin = 0
+                if state[row][col] == '-' or state[row][col] == 'R':
+                    bHoriWin = 0
+                if bHoriWin > rHoriWin:
+                    bSeqTotal += bHoriWin
+                if rHoriWin >= bHoriWin:
+                    rSeqTotal += rHoriWin
+
+        bDiagWin = 0 # diagonal counters
+        rDiagWin = 0
+        ROW = 6
+        COL = 7
+        # There will be 6 + 7 -1 lines in the output
+        # 6 rows 7 cols
+        for line in range(1,(ROW + COL)):
+            # Get column index of the first element
+            # in this line of output. The index is 0
+            # for first ROW lines and line - ROW for
+            # remaining lines
+            start_col = max(0,line - ROW)
+            # Get count of elements in this line.
+            # The count of elements is equal to minimum of line number, COL-start_col and ROW
+            count = min(line,(COL - start_col),ROW)
+            # Print elements of this line
+            for j in range(0, count):
+                if state[min(ROW, line) - j - 1][start_col + j] == 'B':
+                    bDiagWin += 1
+                if state[min(ROW, line) - j - 1][start_col + j] == 'R':
+                    rDiagWin += 1
+                if state[min(ROW, line) - j - 1][start_col + j] == '-' or state[min(ROW, line) - j - 1][start_col + j] == 'R':
+                    bDiagWin = 0
+                if state[min(ROW, line) - j - 1][start_col + j] == '-' or state[min(ROW, line) - j - 1][start_col + j] == 'B':
+                    rDiagWin = 0
+                if bDiagWin > rDiagWin:
+                    bSeqTotal += bDiagWin
+                if rDiagWin > bDiagWin:
+                    rSeqTotal += rDiagWin
+
+    bValTotal += bSeqTotal
+    rValTotal += rSeqTotal
+
+    if(rValTotal > bValTotal):
+        print("Red has the avantage of",rValTotal,"to",bValTotal)
+    if(bValTotal > rValTotal):
+        print("Black has the avantage of",bValTotal,"to",rValTotal)
+    if(bValTotal == rValTotal):
+        print("Neither player has the advantage")
+
+    # heuristic from the point of view of black player
+    return bValTotal - rValTotal
 
 #Entry Point
 print("Welcome to Connect4 Python!")
 print()
 print("What would you like to do?")
-print("1 Player 1 vs Player 2")
-print("2 Evaluate Game State File")
+print("1. Player 1 vs Player 2")
+print("2. Evaluate Game State File")
 choice = input()
 
 if choice == '1':
     print("Player vs Player mode!")
     print()
+    print("Which Evaluation function would you like to use?")
+    print("1. Location Based Value")
+    print("2. Location Based Value and Sequence Based Value")
+    evalChoice = input()
 
     P1 = 'B' # player 1
     P2 = 'R' # player 2
@@ -153,13 +251,21 @@ if choice == '1':
 
         c4GameBoard.makeMove(player1Move,P1)
         c4GameBoard.printGameBoard()
-        c4Eval(c4GameBoard.gameBoard) # prints evaluation results
+        if evalChoice == '2':
+            c4Eval1(c4GameBoard.gameBoard) # prints evaluation results
+        if evalChoice == '1':
+            c4Eval(c4GameBoard.gameBoard) # prints evaluation results
         moveCount += 1
 
-        if(moveCount >= 8):
+        if(moveCount >= 7):
             result = c4GameBoard.checkWin()
             if result != None:
-                print(result)
+                if result == 1000:
+                    print("Black has won")
+                if result == -1000:
+                    print("Red has won")
+                if result == 0:
+                    print("The game has ended in a tie")
                 exit()
         print()
         print("Player 2's Turn")
@@ -170,13 +276,21 @@ if choice == '1':
 
         c4GameBoard.makeMove(player2Move,P2)
         c4GameBoard.printGameBoard()
-        c4Eval(c4GameBoard.gameBoard) # prints evaluation results
+        if evalChoice == '2':
+            c4Eval1(c4GameBoard.gameBoard) # prints evaluation results
+        if evalChoice == '1':
+            c4Eval(c4GameBoard.gameBoard) # prints evaluation results
         moveCount += 1
 
-        if(moveCount >= 8):
+        if(moveCount >= 7):
             result = c4GameBoard.checkWin()
             if result != None:
-                print(result)
+                if result == 1000:
+                    print("Black has won")
+                if result == -1000:
+                    print("Red has won")
+                if result == 0:
+                    print("The game has ended in a tie")
                 exit()
 
 if choice == '2':
